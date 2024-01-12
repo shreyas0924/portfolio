9247add9de52720f426c8ba9b61a6c1804a5a722
shreyas0924 <shreyasp0924@gmail.com>
2024-01-12 22:13:57 +0530

    fixed eslint error on prod

--- components/about.tsx
+++ components/about.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className=' mt-10'>Hey, I'm Shreyas P, a final year CS undergrad. I am a passionate full-stack developer and I like building fun projects and contributing to open source. </div>
+    <div className=' mt-10'>{`Hey, I'm Shreyas P, a final year CS undergrad. I am a passionate full-stack developer and I like building fun projects and contributing to open source.`} </div>
   )
 }
