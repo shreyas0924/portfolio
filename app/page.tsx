33fb068c6f570b559473b340a27e8ceb047e20e6
shreyas0924 <shreyasp0924@gmail.com>
2024-01-12 22:21:30 +0530

    work and projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { About } from "@/components/about";
 import Projects from "@/components/projects";
+import Work from "@/components/work";
 export default function Home() {
   return (
     <main className="">
       <About />
+      <Work />
       <Projects />
     </main>
   )
---
+++ components/work.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div className='mt-10'>
+      <div>Work</div>
+    </div>
+  )
+}
+
+export default Work
