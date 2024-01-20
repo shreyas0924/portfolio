15095e69e22699dafe98d111924f0c5a9cb1d104
shreyas0924 <shreyasp0924@gmail.com>
2024-01-20 16:15:08 +0530

    created project reusable component

---
+++ components/project-card.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Badge } from './ui/badge'
+type ProjectProps = {
+  name: string
+  url: string
+  github: string
+  tech: string[]
+}
+export default function ProjectCard({ name, url, github, tech }: ProjectProps) {
+  return (
+    <div className='border border-gray-500 p-3 rounded-2xl h-full'>
+      <header className=''>
+        <div className='mx-auto flex items-center'>
+          <h1 className='text-xl font-bold'>{name}</h1>
+          <div className='flex ml-auto mr-3 gap-5'>
+            <div className=''>{url}</div>
+            <div>{github}</div>
+          </div>
+        </div>
+      </header>
+      <div className='grid grid-cols-1'>
+        <div className='py-2'>
+          <p className=' mb-4'>
+            An e-commerce platform for books with category-based shopping,
+            wishlist, kart, address checkout, and coupon functionality.
+            Integrated Razorpay for payments.
+          </p>
+          <div className='flex flex-wrap gap-2'>
+            {tech.map((t) => (
+              <Badge>{t}</Badge>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import ProjectCard from './project-card'
 
 const Projects = () => {
   return (
     <main>
       <h3 className='text-xl font-bold mb-4'>Projects</h3>
+      <div className='my-5 flex flex-col gap-4'>
+        <ProjectCard
+          name='Shopwise'
+          github='#'
+          image='#'
+          tech={['TypeScript', 'Next.js', 'TailwindCSS', 'Supabase', 'Prisma']}
+          url='#'
+        />
+        <ProjectCard
+          name='Shopwise'
+          github='#'
+          image='#'
+          tech={['TypeScript', 'Next.js', 'TailwindCSS', 'Supabase', 'Prisma']}
+          url='#'
+        />
+      </div>
     </main>
   )
 }
