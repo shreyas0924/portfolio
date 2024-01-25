58e98871f50d41ce3e3b3e296812e479b0bc65a0
shreyas0924 <shreyasp0924@gmail.com>
2024-01-25 11:54:13 +0530

    nothing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { About } from '@/components/about'
-import Contact from '@/components/contact'
-import Projects from '@/components/projects'
-import Skills from '@/components/skills'
-import Work from '@/components/work'
+import { About } from "@/components/about";
+import Contact from "@/components/contact";
+import Projects from "@/components/projects";
+import Skills from "@/components/skills";
+import Work from "@/components/work";
 export default function Home() {
   return (
-    <main className=''>
+    <main>
       <About />
       <Work />
       <Skills />
       <Projects />
       <Contact />
     </main>
-  )
+  );
 }
